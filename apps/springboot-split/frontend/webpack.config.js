04e14731bf01373aa27db4dc7e55af48c45c76f9
Olaf K-Freund <olaf.loken@google.com>
2024-11-10 13:06:14 +0000

    Add Spring Boot application with React frontend and Docker support; configure application properties and set up WebSocket listener

---
+++ apps/springboot-split/frontend/webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+
+module.exports = {
+	entry: './src/main/js/app.js',
+	devtool: 'sourcemaps',
+	cache: true,
+	mode: 'development',
+	resolve: {
+		alias: {
+			'stompjs': __dirname + '/node_modules' + '/stompjs/lib/stomp.js',
+		}
+	},
+	output: {
+		path: __dirname,
+		filename: './src/main/resources/static/built/bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				// test: path.join(__dirname, '.'),
+				test: /\.(js|jsx)$/,
+				exclude: /(node_modules)/,
+				use: [{
+					loader: 'babel-loader',
+					options: {
+						presets: ["@babel/preset-env", "@babel/preset-react"]
+					}
+				}]
+			}
+		]
+	},
+	devServer: {
+		proxy: {
+			'/api': 'http://localhost:8080'
+		}
+	}
+};
